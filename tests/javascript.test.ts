83a697da3ee32f42c24ae4a3c58424ddec0b05fd
Fynn Grandke <fynn.grandke@web.de>
2020-01-25 16:53:50 +0900

    Expanded the testing algo

--- tests/javascript.test.ts
+++ tests/javascript.test.ts
@@ -12,11 +12,17 @@ function arraysEqual(arr1, arr2) {
   return true;
 }
 
-easyCode.forEach(val => {
-  console.log(val.tests)
+easyCode.forEach(snippet => {
+  snippet.tests.forEach(test => {
+    const result = snippet.code(test.input);
+
+    if (snippet.type === 'Array') assert(arraysEqual(result, test.expect), test.errormsg);
+    else assert(result === test.expect, test.errormsg);
+
+    assert(Array.isArray(result) || typeof result === snippet.type, 'Output type does not match the expected output type');
+  });
 });
 
+console.log('Passed successfully!');
+
 // assert(arraysEqual(func.code(func.tests[0].test), func.tests[0].expect), func.tests[0].errormsg);
-
-
-``
