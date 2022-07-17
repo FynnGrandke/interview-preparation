d95a9a9fb0991ee19f81702a2350785ffcca3d99
FynnGrandke <fynn.grandke@web.de>
2022-07-17 16:49:03 +0900

    Add index.ts file; Fix tsconfig

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// You can write your code here.
+// Add console.log(`${myVariable}`) to see your output.
+// Don't forget to run 'npm start' in the console!
+
+const myVariable = 'World'
+
+console.log(`Hello ${myVariable}`)
